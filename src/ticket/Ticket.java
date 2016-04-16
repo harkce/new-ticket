f3f50d80c316d8e219e9ecf3786f3f6943158d01
harkce <habibgarut@gmail.com>
2016-04-16 12:50:19 +0700

    First Commit

---
+++ src/ticket/Ticket.java
@@ -0,0 +1,21 @@
+/*
+ * Created by Muhammad Habib Fikri Sundayana
+ * habibgarut@gmail.com | http://edufi.id/ | http://cingkleung.com
+ * Don't lazy to think, feel free to think lazy :)
+ */
+package ticket;
+
+/**
+ *
+ * @author habib
+ */
+public class Ticket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
