c3fe446eff1efefc3985256fbcb7df2da7e642de
Ridwan Wibowo <Ridwan Wibowo@RIDWAN-PC>
2016-04-19 23:42:12 +0700

    Controller

---
+++ src/controller/ControlAdminMainForm.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author Ridwan Wibowo
+ */
+
+import model.Stasiun;
+import model.Gerbong;
+import model.Kereta;
+import model.Rute;
+import model.Tiket;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import javax.swing.JOptionPane;
+import view.InputStasiun;
+public class ControlAdminMainForm {
+    
+}
